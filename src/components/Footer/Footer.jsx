0ac376148e5ee6b4852677c5aa2ea30c07c90c81
Amit-TheOne <amit.dew009@gmail.com>
2024-03-05 21:24:19 +0530

    Updated: Footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -8,21 +8,21 @@ const Footer = () => {
                 <div className="flexColStart f-left">
                     <img src="/src/assets/logo2.png" alt="Logo" width={120} />
                     <span className="secondaryText">
-                        Our vision is to make all people <br />
-                        the best place to live for them.
+                        Providing the best possible Property <br />
+                        for everyone to live in.
                     </span>
                 </div>
 
                 {/* right side */}
                 <div className="flexColStart f-right">
-                    <span className="primaryText">Information</span>
+                    <span className="primaryText">Address</span>
                     <span className="secondaryText">
                         145 New York, FL 5467, USA
                     </span>
                     <div className="flexCenter f-menu">
                         <span>Property</span>
                         <span>Services</span>
-                        <span>Product</span>
+                        <span>Blog</span>
                         <span>About Us</span>
                     </div>
                 </div>
